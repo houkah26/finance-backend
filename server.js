d3358a47af5a86650621655d6f42cb3d243b6d74
houkah26 <houkah26@gmail.com>
2017-07-31 14:57:44 -0700

    prettier formatting

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express'),  
-      app = express(),
-      bodyParser = require('body-parser'),
-      logger = require('morgan'),
-      mongoose = require('mongoose'),
-      config = require('./config/main')
-      router = require('./router');
+const express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  logger = require("morgan"),
+  mongoose = require("mongoose"),
+  config = require("./config/main");
+router = require("./router");
 
 // Database connection
 mongoose.Promise = global.Promise;
@@ -15,13 +15,16 @@ const server = app.listen(config.port);
 console.log(`Your server is running on port ${config.port}.`);
 
 // Setting up basic middleware for all Express requests
-app.use(logger('dev')); // Log requests to console using morgan
+app.use(logger("dev")); // Log requests to console using morgan
 
 // Enable CORS from client-side
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials"
+  );
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });

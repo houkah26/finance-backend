4abb138bb1668dc0f5d0042f34bdca28c52bd344
houkah26 <houkah26@gmail.com>
2017-07-31 14:58:15 -0700

    set mlab mongdb URI when NODE_ENV == production

--- config/main.js
+++ config/main.js
@@ -1,10 +1,15 @@
+const database =
+  process.env.NODE_ENV === "production"
+    ? "mongodb://heroku_6h410gcz:5hmnad1jg9kak433luvo82ccs6@ds129043.mlab.com:29043/heroku_6h410gcz"
+    : "mongodb://localhost:27017";
+
 module.exports = {
   // Secret key for the JWT signing and encyrption
-  'secret': '3 ducks walked into an elephant',
+  secret: "3 ducks walked into an elephant",
   // Database connection
-  'database': 'mongodb://localhost:27017',
+  database: database,
   // Setting port for the server
-  'port': process.env.PORT || 3001,
+  port: process.env.PORT || 3001,
   // Alpha Vantage stock quote API key
-  'stockAPIkey': 'IT717SBUULYDIMTY'
-}
+  stockAPIkey: "IT717SBUULYDIMTY"
+};
